3ee5f9f01cae32331adbdc98978d1de9784dd263
kensen <kensen@foxmail.com>
2015-02-07 08:41:07 +0800

    up

---
+++ WeiDev/Models/EntityConfigurations/YEFUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YEF.Infrastructure.Data;
+
+namespace YEF.Models.EntityConfigurations
+{
+    public class YEFUserConfiguration:EntityConfigurationBase<YEFUser,Guid>
+    {
+        public YEFUserConfiguration()
+        {
+            HasOptional(m => m.YEFRole).WithMany(n => n.YEFUsers);
+            HasOptional(m => m.YEFOrganization).WithMany(n => n.YEFUsers);
+        }
+    }
+}
